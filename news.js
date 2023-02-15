adacad5f0c1f4d434680ef4ee8e18710546921e3
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2023-02-15 17:18:31 +0700

    Add files via upload

--- news.js
+++ news.js
@@ -5,8 +5,20 @@ fetch('./data.json')
 let news = [];
 
 let btn = document.getElementsByClassName('btna')
-
-function addshow(x){
-    
-  //  window.location.href = "./news.html"
-}
+function addshow(id) {
+    fetch('./data.json')
+       .then((response) => response.json())
+       .then((data) => {
+          // Tìm tin tức có id tương ứng
+          const news = data.news.find((item) => item.id === id);
+          
+          // Hiển thị nội dung của tin tức
+          const newsContainer = document.getElementById('news-container');
+          newsContainer.innerHTML = `
+             <h2>${news.title}</h2>
+             <p>${news.content}</p>
+          `;
+       });
+       //window.location.href = "./news.html"
+ }
+ 
